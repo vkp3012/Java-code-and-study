c071ad7c2c93c62cc3bea653af1a2524a8485469
vkp3012 <vivekpatel301295@gmail.com>
2022-05-27 21:27:48 +0530

    generic tree question solve

--- code/GenericTree/construction_and_display.java
+++ code/GenericTree/construction_and_display.java
@@ -45,8 +45,8 @@ public class construction_and_display {
   }
 
   public static void main(String[] args){
-    int arr[]={10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
-    Node root=construct(arr);
+    int arr[] = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
+    Node root = construct(arr);
     display(root);
   }
   
