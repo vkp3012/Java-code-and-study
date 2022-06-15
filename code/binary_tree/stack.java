25d0f61f0191d19c6724d1941d2990d026f6a64b
vkp3012 <vivekpatel301295@gmail.com>
2022-06-15 12:59:22 +0530

    :technologist: height of binary tree is done

--- code/binary_tree/stack.java
+++ code/binary_tree/stack.java
@@ -1,7 +1,18 @@
 
 public class stack<T> {
 
-    public void push(max_binary_tree.Pair rtp) {
+    public void push(height_binary_tree.Pair rtp) {
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public height_binary_tree.Pair peek() {
+        return null;
+    }
+
+    public void pop() {
     }
 
 }
