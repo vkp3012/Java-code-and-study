e2c07c008b9748f1ab9adf45d91c580285fc6e38
vkp3012 <vivekpatel301295@gmail.com>
2022-06-26 13:23:56 +0530

    all get element contains is hashmap question is done

--- code/hashmap/get_element_contains1.java
+++ code/hashmap/get_element_contains1.java
@@ -1,7 +1,6 @@
 import java.util.HashMap;
 
 // Get Common Elements - 1
-// Easy
 
 // 1. You are given a number n1, representing the size of array a1.
 // 2. You are given n1 numbers, representing elements of array a1.
@@ -27,42 +26,19 @@ import java.util.HashMap;
 // Example
 // Sample Input
 
-// 9
-// 5
-// 5
-// 9
-// 8
-// 5
-// 5
-// 8
-// 0
-// 3
-// 18
-// 9
-// 7
-// 1
-// 0
-// 3
-// 6
-// 5
-// 9
-// 1
-// 1
-// 8
-// 0
-// 2
-// 4
-// 2
-// 9
-// 1
-// 5
+//n1 ->  9
+// array1 ---> {5,5,9,8,5,5,8,0,3}
 
-// Sample Output
-// 9
-// 0
-// 3
-// 5
-// 8
+//n2 ---> 18
+
+// array2 --->  {9,7,1,0,3,6,5,9,1,1,8,0,2,4,2,9,1,5}
+
+// Sample Output ->
+                    // 9
+                    // 0
+                    // 3
+                    // 5
+                    // 8
 
 
 public class get_element_contains1{
--- code/hashmap/get_element_contains2.java
+++ code/hashmap/get_element_contains2.java
@@ -1,9 +1,15 @@
 import java.util.HashMap;
 
 // Get Common Elements - 2
-// Easy
 
-// 1. You are given a number n1, representing the size of array a1. 2. You are given n1 numbers, representing elements of array a1. 3. You are given a number n2, representing the size of array a2. 4. You are given n2 numbers, representing elements of array a2. 5. You are required to find the intersection of a1 and a2. To get an idea check the example below: if a1 -> 1 1 2 2 2 3 5 and a2 -> 1 1 1 2 2 4 5 intersection is -> 1 1 2 2 5 Note -> Don't assume the arrays to be sorted. Check out the question video.
+// 1. You are given a number n1, representing the size of array a1. 
+// 2. You are given n1 numbers, representing elements of array a1. 
+// 3. You are given a number n2, representing the size of array a2. 
+// 4. You are given n2 numbers, representing elements of array a2. 
+// 5. You are required to find the intersection of a1 and a2. 
+//     To get an idea check the example below: 
+//         if a1 -> 1 1 2 2 2 3 5 and a2 -> 1 1 1 2 2 4 5 intersection is -> 1 1 2 2 5 
+//         Note -> Don't assume the arrays to be sorted. 
 
 
 // Constraints
@@ -22,24 +28,12 @@ import java.util.HashMap;
 // Example
 // Sample Input
 
-// 7
-// 1
-// 1
-// 2
-// 2
-// 2
-// 3
-// 5
+//n1 -->  7
 
-// 7
+//array1 --> {1,1,2,2,2,3,5}
 
-// 1
-// 1
-// 1
-// 2
-// 2
-// 4
-// 5
+//n2 --> 7
+//array2 --> {1,1,1,2,2,4,5}
 
 // Sample Output
 // 1
