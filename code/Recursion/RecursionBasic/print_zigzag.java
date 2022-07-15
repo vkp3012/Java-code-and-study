33edbb9414d868b6db7b6d8100db922a1208f4de
vkp3012 <vivekpatel301295@gmail.com>
2022-07-16 04:11:04 +0530

    :fire: print zig-zag number print using recursion

---
+++ code/Recursion/RecursionBasic/print_zigzag.java
@@ -0,0 +1,59 @@
+// Print Zigzag
+// Easy
+
+// 1. Here are a few sets of inputs and outputs for your reference
+// Input1 -> 1
+// Output1 -> 1 1 1
+
+// Input2 -> 2
+// Output2 -> 2 1 1 1 2 1 1 1 2
+
+// Input2 -> 3
+// Output3 -> 3 2 1 1 1 2 1 1 1 2 3 2 1 1 1 2 1 1 1 2 3
+
+// 2. Figure out the pattern and complete the recursive function pzz to achieve the above for any positive number n.
+
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is.Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+// Constraints
+// 1 <= n <= 10
+
+// Format
+// Input
+// A number n
+
+// Output
+// As discussed in point 1 of description
+
+// Example
+// Sample Input
+
+// 3
+
+// Sample Output
+// 3 2 1 1 1 2 1 1 1 2 3 2 1 1 1 2 1 1 1 2 3
+
+
+package RecursionBasic;
+
+public class print_zigzag{
+
+    public static void main(String args[]){
+        int n = 2;
+
+        zigzag(n);
+    }
+
+    public static void zigzag(int n){
+        if(n==0){
+            return;
+        }
+
+        System.out.println(n + " ");
+        zigzag(n-1);
+        System.out.println(n + " ");
+        zigzag(n-1);
+        System.out.println(n + " ");
+        
+    }
+}
