95eddd15a118060a6bf4dedddbb594f0c06ce9f2
vkp3012 <vivekpatel301295@gmail.com>
2022-07-29 14:46:27 +0530

    string syntex checker is done

--- code/string/string.java
+++ code/string/string.java
@@ -10,8 +10,12 @@ public class string{
         //     System.out.println(ch);
         // }
         for(int i = 0;i<s1.length();i++){
-            char ch = s1.charAt(i);
-            String[] arr = ch.
+            char[] ch = new char[s1.charAt(i)];
+            for (; i < ch.length; i++) {
+                ch[i] = (char) ch.length;
+            }
+            // char[] ch = new char[str.length()];
+            // String[] arr = ch.
             System.out.println(ch);
         }
     }
